abec1f5c236185ee0db93f519622abbe2f6ba2e0
Chris <Chris@CHRIS-HP>
2016-06-20 00:09:48 +0900

    first

--- MyDelivery/MainActivity.cs
+++ MyDelivery/MainActivity.cs
@@ -22,8 +22,6 @@ namespace MyDelivery
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.MyButton);
 
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
